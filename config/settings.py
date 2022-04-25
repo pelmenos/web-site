a44150e76fd7aa6e6ed9c390b8a0a531f95c8adb
pelmenos <vadimshvynda@gmail.com>
2022-04-26 00:58:25 +0700

    WorkSad

--- config/settings.py
+++ config/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 import dj_database_url
 db_from_env = dj_database_url.config()
